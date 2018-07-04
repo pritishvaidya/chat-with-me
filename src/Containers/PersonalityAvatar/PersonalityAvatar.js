c6db65c93409c7b8d541199cbe2901b3159352ff
pritishvaidya <pritishvaidya94@gmail.com>
2018-07-04 19:34:45 +0530

    Adding the first blink animation

--- src/Containers/PersonalityAvatar/PersonalityAvatar.js
+++ src/Containers/PersonalityAvatar/PersonalityAvatar.js
@@ -2,23 +2,69 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
-import {Colors} from "../../Theme/Global";
+import { Colors } from '../../Theme/Global';
+import Utils from '../../Utils';
 
-import Professional from '../../Assets/Professional'
+import Professional from '../../Assets/Professional';
 
-type Props = {}
-type State = {}
+type Props = {};
+type State = {
+    animationSequenceImage: any,
+};
 
 class PersonalityAvatar extends Component<Props, State> {
-    state = {};
+    constructor(props) {
+        super(props);
+        this.state = {
+            animationSequenceImage: Professional.Normal,
+        };
+        this.currentAnimationSequence = null;
+        this.animationImages = null;
+    }
 
-    render () {
-      return (
-        <AvatarWrapper ref={'avatar'}>
-          <ImageBackground src={Professional.Background}/>
-          <Image src={Professional.Normal}/>
-        </AvatarWrapper>
-      )
+    componentDidMount() {
+        this.blink();
+    }
+
+    changeAnimationSequence = (seq, start) => {
+        this.setState({
+            animationSequenceImage:
+                seq === 0
+                    ? Professional.Normal
+                    : Professional.Blinking[seq - 1],
+        });
+    };
+
+    // Animations
+    blink = () => {
+        const delay = Utils.delay;
+        const change = this.changeAnimationSequence;
+        // If any animation already running
+        if (this.currentAnimationSequence) {
+            this.currentAnimationSequence.cancel();
+        }
+
+        this.animationImages = Professional.Blinking;
+        this.currentAnimationSequence = delay(5000, () => change(0, true))
+            .delay(75, () => change(1))
+            .delay(75, () => change(2))
+            .delay(75, () => change(3))
+            .delay(75, () => change(4))
+            .delay(250, () => change(3))
+            .delay(75, () => change(2))
+            .delay(75, () => change(1))
+            .delay(75, () => change(0))
+            .delay(75, () => this.blink());
+    };
+
+    render() {
+        console.log(this.state.animationSequenceImage);
+        return (
+            <AvatarWrapper ref={'avatar'}>
+                <ImageBackground src={Professional.Background} />
+                <Image src={this.state.animationSequenceImage} />
+            </AvatarWrapper>
+        );
     }
 }
 
@@ -33,17 +79,17 @@ const AvatarWrapper = styled.div`
   flex: 1;
   background-color: ${Colors.Background}
   position: relative
-`
+`;
 
 const ImageBackground = styled.img`
   height: 600px;
   width: 100%
   filter: blur(10px);
-`
+`;
 
 const Image = styled.img`
   position: absolute;
   bottom: 0
   height: 600px
   left: 40%
-`
+`;
---
+++ src/Utils/index.js
@@ -0,0 +1,41 @@
+const functions = {
+  random: (min, max) => {
+    return Math.floor(Math.random() * max) + min
+  },
+  delay: (t, fn) => {
+    // private instance variables
+    var queue = [], self, timer;
+
+    function schedule(t, fn) {
+      timer = setTimeout(function() {
+        timer = null;
+        fn();
+        if (queue.length) {
+          var item = queue.shift();
+          schedule(item.t, item.fn);
+        }
+      }, t);
+    }
+    self = {
+      delay: function(t, fn) {
+        // if already queuing things or running a timer,
+        //   then just add to the queue
+        if (queue.length || timer) {
+          queue.push({t: t, fn: fn});
+        } else {
+          // no queue or timer yet, so schedule the timer
+          schedule(t, fn);
+        }
+        return self;
+      },
+      cancel: function() {
+        clearTimeout(timer);
+        queue = [];
+        return self;
+      }
+    };
+    return self.delay(t, fn);
+  }
+}
+
+export default functions
